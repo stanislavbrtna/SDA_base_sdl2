0a90318b5919f77bd08dde82d98a2019b63f3e63
standa <stanislav.brtna@gmail.com>
2023-11-20 01:23:23 +0100

    crypto status and gc collect added

--- sda_info_window.c
+++ sda_info_window.c
@@ -42,24 +42,55 @@ uint8_t info_window_loop(uint8_t touch, uint32_t mouse_x, uint32_t mouse_y) {
   static uint8_t touchPrev;
   gr2EventType touchType = EV_NONE;
   uint8_t touchValid;
+
+  // gr2 screen elements
   static uint16_t string_bar;
   static uint16_t app_name;
   static uint16_t reset_button;
+  static uint16_t collect_button;
+  static uint16_t lock_txt;
+
+  static uint16_t elements_bar;
   
   if (info_window_init == 0) {
     gr2_init_context(&c_info, info_elements, 40, info_screens, 10);
     gr2_reset_context(&c_info);
+
+    gr2_set_border_color(gr2_get_border_color(&sda_sys_con), &c_info);
+    gr2_set_text_color(gr2_get_text_color(&sda_sys_con), &c_info);
+    gr2_set_background_color(gr2_get_background_color(&sda_sys_con), &c_info);
+    gr2_set_fill_color(gr2_get_fill_color(&sda_sys_con), &c_info);
+    gr2_set_active_color(gr2_get_active_color(&sda_sys_con), &c_info);
+
     scr = gr2_add_screen(&c_info);
     gr2_set_relative_init(1, &c_info);
 
-    gr2_add_text(1, 0, 5, 1, "Sim Info", scr, &c_info);
+    uint16_t line = 0;
 
-    app_name = gr2_add_text(1, 1, 10, 1, "", scr, &c_info);
+    gr2_add_text(1, line, 5, 1, "Sim Info", scr, &c_info);
 
-    gr2_add_text(1, 2, 5, 1, "String mem:", scr, &c_info);
-    string_bar = gr2_add_progbar_v(5, 2, 5, 1, svm.stringFieldMax, svm.stringFieldLen, scr, &c_info);
+    line++;
 
-    reset_button = gr2_add_button(1, 4, 5, 1, "Reload app", scr, &c_info);
+    app_name = gr2_add_text(1, line, 15, 1, "", scr, &c_info);
+
+    line++;
+
+    gr2_add_text(1, line, 5, 1, "String mem:", scr, &c_info);
+    string_bar = gr2_add_progbar_v(6, line, 5, 1, svm.stringFieldMax, svm.stringFieldLen, scr, &c_info);
+
+    collect_button = gr2_add_button(12, 2, 5, 1, "GarbageCollect", scr, &c_info);
+
+    line++;
+
+    gr2_add_text(1, line, 6, 1, "GR2 elements:", scr, &c_info);
+    elements_bar = gr2_add_progbar_v(6, line, 5, 1, sda_app_con.elementsMax, sda_app_con.elementsUsed, scr, &c_info);
+
+    lock_txt = gr2_add_text(12, line, 6, 1, "N/A", scr, &c_info);
+
+    line++;
+    line++;
+
+    reset_button = gr2_add_button(1, 5, 5, 1, "Reload app", scr, &c_info);
     info_window_init = 1;
   }
 
@@ -100,7 +131,13 @@ uint8_t info_window_loop(uint8_t touch, uint32_t mouse_x, uint32_t mouse_y) {
 
   if (svmGetValid()) {
     gr2_set_value(string_bar, svm.stringFieldLen, &c_info);
+    gr2_set_value(elements_bar, sda_app_con.elementsUsed, &c_info);
     gr2_set_str(app_name, svmGetName(), &c_info);
+    if(svp_crypto_get_lock()) {
+      gr2_set_str(lock_txt, "Crypto unlocked", &c_info);
+    } else {
+      gr2_set_str(lock_txt, "Crypto locked", &c_info);
+    }
   }
 
   if(gr2_get_event(reset_button, &c_info) == EV_RELEASED) {
@@ -110,6 +147,13 @@ uint8_t info_window_loop(uint8_t touch, uint32_t mouse_x, uint32_t mouse_y) {
     svmLaunch(namebuff, 0);
   }
   gr2_clear_event(reset_button, &c_info);
+
+
+  if(gr2_get_event(collect_button, &c_info) == EV_RELEASED) {
+    garbageCollect(-1, &svm);
+    gr2_set_value(string_bar, svm.stringFieldLen, &c_info);
+  }
+  gr2_clear_event(collect_button, &c_info);
 
   LCD_getDrawArea(&a);
   LCD_setDrawArea(0, 0, INFO_WIDTH, INFO_HEIGHT);
